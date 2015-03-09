80a14758e4bece278ec9568882213d33262d6773
DANIEL SEATON <dseaton@DANIELs-iMac.local>
2015-03-09 17:36:07 -0400

    Refactored two jsxgraph vizs for Macro - Moving Toward Long Term Equilibrium.

---
+++ Macroeconomics/Moving_to_Long_Run_Equlibrium/LC2/MLE_LC2_HOA_1a_static.js
@@ -0,0 +1,54 @@
+////////////
+// BOARD 1
+////////////
+bboxlimits = [-1.5, 12, 12, -1];
+var brd1 = JXG.JSXGraph.initBoard('jxgbox1', {axis:false, 
+                                        showCopyright: false,
+                                        showNavigation: false,
+                                        zoom: false,
+                                        pan: false,
+                                        boundingbox:bboxlimits,
+                                        grid: false,
+                                        hasMouseUp: true, 
+});
+
+xaxis1 = brd1.create('axis', [[0, 0], [11, 0]], {withLabel: false});
+yaxis1 = brd1.create('axis', [[0, 0], [0, 11]], {withLabel: false});
+
+//Axes
+xaxis1.removeAllTicks();
+yaxis1.removeAllTicks();
+var xlabel1 = brd1.create('text',[-1.2,10,"PL"],{fixed:true});
+var ylabel1 = brd1.create('text',[9,-0.5,"RGDP"],{fixed:true});
+
+//Supply Line 1 - fixed
+var Supply = createSupply(brd1,{name:'SRAS',color:'DodgerBlue'});
+Supply.setAttribute({'name':'SRAS','fixed':true,'highlight':false});
+ 
+//Demand Line 2 - fixed
+var AD2 = createDemand(brd1,{name:'AD2',color:'Orange'});
+AD2.setAttribute({fixed:true,withLabel:false,'highlight':false});    
+
+////////////
+// Intersection Box 1
+////////////
+var iS2D = brd1.create('intersection', [AD2, Supply, 0], {visible:false});
+
+////////////
+// Fixed Dashed Lines for Board 1
+////////////
+var dashS2 = createDashedLines2Axis(brd1,iS2D,
+                                  {fixed:true,
+                                   withLabel:true,
+                                   xlabel:'Y1',
+                                   ylabel:'PL1',
+                                   color:'DodgerBlue'});
+
+////////////
+//LRAS - straight line
+////////////
+var LRAS = brd1.create('segment',[[7.0,11.0],[7.0,0.0]],
+                       {'strokeColor':'DodgerBlue','strokeWidth':'2',
+                        'name':'LRAS','withLabel':true,'highlight':false,
+                        'label':{'offset':[-15,140]}});
+var labelLRAS = brd1.create('text',[6.7,-0.4,"rYF"],{fixed:true});
---
+++ Macroeconomics/Moving_to_Long_Run_Equlibrium/LC2/MLE_LC2_HOA_1a_static.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# ###Moving to Long-Run Equilibrium
+# https://studio.edge.edx.org/container/i4x://DavidsonCollege/DAP002/vertical/ee567d7e52294aeaa8a32cd1c3f09cf3?action=new
+
+# In[4]:
+
+get_ipython().run_cell_magic(u'HTML', u'', u'<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset="UTF-8">\n        <title>Moving Toward Long-Run Equilibrium</title>\n        <script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=AM_HTMLorMML-full"></script>\n        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jsxgraph/0.98/jsxgraphcore.js"></script>\n        <script type="text/javascript" src="underscore-min.js"></script>\n        <script type="text/javascript" src="jquery.min.js"></script>\n        <script type="text/javascript" src="jquery-ui.min.js"></script>\n        <script type="text/javascript" src="../../JS/Macro.js"></script>\n        <script type="text/javascript" src="MLE_LC2_HOA_1a_static.js"></script>\n    </head>\n\n    <body>\n        <div style="width: 100%; overflow: hidden;">\n            <div id=\'jxgbox1\' class=\'jxgbox\' style=\'width:350px; height:300px; float:left; border: solid #1f628d 2px;\'></div>        \n        </div>\n    </body>\n</html>')
+
+
+# ### Generate HTML File
+
+# In[5]:
+
+import re
+
+#tmpfile = _i86
+index_htmlinput = [ i for i,x in enumerate(_ih) if "run_cell_magic(u'HTML'" in x and "re.sub('%%HTML','',tmpfile)" not in x]
+
+tmpfile = eval('_i%d' % int(index_htmlinput[-1]))
+tmpfile = re.sub('%%HTML','',tmpfile)
+tmpfile = re.sub(r'<!--START-BUTTON FOR PASS STATE(.*?)END-BUTTON FOR PASS STATE-->','',tmpfile,flags=re.DOTALL)
+tmpfile = re.sub(r'//START-PASS STATE TO IPYTHON KERNEL(.*?)//END-PASS STATE TO IPYTHON KERNEL','',tmpfile,flags=re.DOTALL)
+tmpfile = re.sub(r'<script type="text/javascript" src="../../JS/Macro.js"></script>','<script type="text/javascript" src="/static/Macro.js"></script>',tmpfile,flags=re.DOTALL)
+
+filename = 'MLE_LC2_HOA_1a_static'
+html_filename = '%s.html' % filename
+
+with open(html_filename,'w') as hfile:
+    hfile.write(tmpfile)
+print tmpfile
+
+
+# In[ ]:
+
+
+
