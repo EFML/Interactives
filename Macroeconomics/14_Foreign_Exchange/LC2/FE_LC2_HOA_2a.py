0a2dca8e487fd51c5624be3950931164f98cf717
Seaton <daseaton@w15126.davidson.edu>
2015-05-19 21:35:14 -0400

    Weird clean up. Also added activities for Foreign Exchange.

--- Macroeconomics/14_Foreign_Exchange/LC2/FE_LC2_HOA_2a.py
+++ Macroeconomics/14_Foreign_Exchange/LC2/FE_LC2_HOA_2a.py
@@ -4,7 +4,7 @@
 # ###Foreign Exchange- LC2 HOA 2a
 # https://studio.edge.edx.org/container/i4x://DavidsonCollege/DAP002/vertical/a2c421726ee4432e8cad65be3c0491ad
 
-# In[7]:
+# In[3]:
 
 import sys
 import re
@@ -23,14 +23,14 @@ tGen = templateGenerator.templateGenerator(course_id='DavidsonCollege/DAP002/3T2
 
 # ### HTML Interactive Cell
 
-# In[9]:
+# In[4]:
 
 get_ipython().run_cell_magic(u'HTML', u'', u'<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset="UTF-8">\n        <title></title>\n        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jsxgraph/0.98/jsxgraphcore.js"></script>\n    </head>\n\n    <body>\n        <div style="width: 100%; overflow: hidden;">\n            <div id=\'jxgbox1\' class=\'jxgbox\' style=\'width:500px; height:450px; float:left; border: solid #1f628d 2px;\'></div>        \n        </div>\n        \n        <!--START-BUTTON FOR PASS STATE-->\n        <div id=\'StateGrab\' style=\'width:350px; float:left;\'>        \n            <input class="btn" type="button" value="Get State" onClick="getNotebookState()">\n            <div id="spaceBelow">State:</div>\n        </div>\n        <script type="text/javascript">\n            getNotebookState = function(){\n                state = getGrade();\n                statestr = JSON.stringify(state);\n\n                document.getElementById(\'spaceBelow\').innerHTML += \'<br>\'+statestr;\n                var command = "state = " + statestr;\n                console.log(command);\n\n                //Kernel\n                var kernel = IPython.notebook.kernel;\n                kernel.execute(command);\n\n                return statestr;\n            }\n        </script>\n        <!--END-BUTTON FOR PASS STATE-->\n        \n        <script type="text/javascript" src="../../JS/jschannel.js"></script>\n        <script type="text/javascript" src="../../JS/edxintegration.js"></script>\n        <script type="text/javascript" src="../../JS/params1Board.js"></script>\n        <script type="text/javascript" src="../../JS/MacroAllBoards.js"></script>\n        <script type="text/javascript" src="FE_LC2_HOA_2a.js"></script>\n    </body>\n</html>')
 
 
 # ### Generate HTML File
 
-# In[10]:
+# In[5]:
 
 # reload(templateGenerator)
 inputCell = eval('_i%d' % tGen.findIPythonHTMLCell(_ih))
