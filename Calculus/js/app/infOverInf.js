0d8578045f96576de89166c7b19d7c67e98974de
Jean-Michel Claus <jmc@math.mit.edu>
2015-06-02 15:23:16 +0200

    Create our own ticks and labels.

--- Calculus/js/app/infOverInf.js
+++ Calculus/js/app/infOverInf.js
@@ -2,8 +2,8 @@ var DataEntry = (function($, _, JXG, undefined) {
     'use strict';
 
     var fGraphBoundingBox = [-10.0, 10, 10.0, -10.0], gGraphBoundingBox = [-10.0, 10, 10.0, -10.0],
-        x0 = 1.0, x0Min = 0.0, x0Max = 500000.0, x0Step = 1000.0, fGraphxAxis, fGraphyAxis, gGraphxAxis, gGraphyAxis, halfSpan = 10.0, precision = 6,
-        fGraphBoard, gGraphBoard, x0Slider, x0SliderValue, fGraphBoundingBoxValue, gGraphBoundingBoxValue,
+        x0 = 1.0, x0Min = 0.0, x0Max = 6.0, x0Step = 0.01, fGraphxAxisTicks = [], fGraphyAxisTicks = [], gGraphxAxis, gGraphyAxis, halfSpan = 10.0, precision = 6,
+        fGraphBoard, gGraphBoard, x0Slider, x0SliderValue, fGraphBoundingBoxValue, gGraphBoundingBoxValue, jm,
 
         fFn = [f0],
         dfFn = [df0],
@@ -83,14 +83,10 @@ var DataEntry = (function($, _, JXG, undefined) {
             min: x0Min,
             max: x0Max,
             step: x0Step,
-            value: x0,
-            slide: function(event, ui ) {
-                var val = ui.value;
-                if (val === 0.0) {
-                    val = 1.0;
-                }
-                x0SliderValue.html(val);
-                x0 =val;
+            value: 0,
+            slide: function(event, ui) {
+                x0 = Math.pow(10.0, ui.value);
+                x0SliderValue.html(x0.toFixed(2));
                 setGraphsBounds();
                 outputDynamicMath();
             }
@@ -131,20 +127,66 @@ var DataEntry = (function($, _, JXG, undefined) {
     }
 
     function setGraphsBounds() {
-        fGraphBoundingBox = [x0 - halfSpan, f(x0) + halfSpan, x0 + halfSpan, f(x0) - halfSpan];
+        var x0Round, y0Round, y0, xMin, xMax, yMin, yMax;
+
+        y0 = f(x0);
+        xMin = x0 - halfSpan;
+        xMax = x0 + halfSpan;
+        yMin = y0 - halfSpan;
+        yMax = y0 + halfSpan;
+
+        fGraphBoundingBox = [xMin, yMax, xMax, yMin];
         fGraphBoard.setBoundingBox(fGraphBoundingBox);
-        fGraphxAxis.point1.moveTo([x0 - halfSpan, f(x0) - halfSpan + 1]);
-        fGraphxAxis.point2.moveTo([x0 + halfSpan, f(x0) - halfSpan + 1]);
-        fGraphyAxis.point1.moveTo([x0 - halfSpan + 1, f(x0) - halfSpan]);
-        fGraphyAxis.point2.moveTo([x0 - halfSpan + 1, f(x0) + halfSpan]);
-        fGraphBoard.fullUpdate();
+
+        // x-axis
+        var xTickMax = xMax - xMax%5.0;
+        var xTick2 = xTickMax - 5.0;
+        var xTick1 = xTickMax - 10.0;
+        var xTickMin = xTickMax - 15.0;
+
+
+        fGraphxAxisTicks[0].point1.moveTo([xTickMax, yMin]);
+        fGraphxAxisTicks[0].point2.moveTo([xTickMax, yMax]);
+
+        fGraphxAxisTicks[1].point1.moveTo([xTick2, yMin]);
+        fGraphxAxisTicks[1].point2.moveTo([xTick2, yMax]);
+
+        fGraphxAxisTicks[2].point1.moveTo([xTick1, yMin]);
+        fGraphxAxisTicks[2].point2.moveTo([xTick1, yMax]);
+
+        fGraphxAxisTicks[3].point1.moveTo([xTickMin, yMin]);
+        fGraphxAxisTicks[3].point2.moveTo([xTickMin, yMax]);
+
+        // y-axis
+        var yTickMax = yMax - yMax%5.0;
+        var yTick2 = yTickMax - 5.0;
+        var yTick1 = yTickMax - 10.0;
+        var yTickMin = yTickMax - 15.0;
+
+        fGraphyAxisTicks[0].point1.moveTo([xMin, yTickMax]);
+        fGraphyAxisTicks[0].point2.moveTo([xMax, yTickMax]);
+
+        fGraphyAxisTicks[1].point1.moveTo([xMin, yTick2]);
+        fGraphyAxisTicks[1].point2.moveTo([xMax, yTick2]);
+
+        fGraphyAxisTicks[2].point1.moveTo([xMin, yTick1]);
+        fGraphyAxisTicks[2].point2.moveTo([xMax, yTick1]);
+
+        fGraphyAxisTicks[3].point1.moveTo([xMin, yTickMin]);
+        fGraphyAxisTicks[3].point2.moveTo([xMax, yTickMin]);
+
+        //fGraphyAxis.point1.moveTo([xMin - 1, yMin]);
+        //fGraphyAxis.point2.moveTo([xMin - 1, yMax]);
+        //fGraphBoard.fullUpdate();
 
         gGraphBoundingBox = [x0 - halfSpan, g(x0) + halfSpan, x0 + halfSpan, g(x0) - halfSpan];
         gGraphBoard.setBoundingBox(gGraphBoundingBox);
-        gGraphxAxis.point1.moveTo([x0 - halfSpan, g(x0) - halfSpan + 1]);
+        /*
+        gGraphxAxis.point1.moveTo([x0 - halfSpan + 5, g(x0) - halfSpan + 1]);
         gGraphxAxis.point2.moveTo([x0 + halfSpan, g(x0) - halfSpan + 1]);
-        gGraphyAxis.point1.moveTo([x0 - halfSpan + 1, g(x0) - halfSpan]);
-        gGraphyAxis.point2.moveTo([x0 - halfSpan + 1, g(x0) + halfSpan]);
+        gGraphyAxis.point1.moveTo([x0 - halfSpan, g(x0) - halfSpan]);
+        gGraphyAxis.point2.moveTo([x0 - halfSpan, g(x0) + halfSpan]);
+        */
         gGraphBoard.fullUpdate();
     }
 
@@ -179,6 +221,9 @@ var DataEntry = (function($, _, JXG, undefined) {
 
     function createfGraphBoard() {
         JXG.Options.axis.ticks.insertTicks = false;
+        JXG.Options.axis.ticks.majorHeight = 100; // -1
+        JXG.Options.axis.ticks.ticksDistance = 5;
+
         fGraphBoard = JXG.JSXGraph.initBoard('height-graph-board', {
             boundingbox: fGraphBoundingBox,
             axis: false,
@@ -189,18 +234,247 @@ var DataEntry = (function($, _, JXG, undefined) {
             grid: false
         });
 
-        fGraphxAxis = fGraphBoard.create('axis', [[0.0, 0.0], [1.0, 0.0]], {
-            withLabel: false,
-            grid: false
-        });
-        fGraphxAxis.removeAllTicks();
+        var xMin = fGraphBoundingBox[0], xMax = fGraphBoundingBox[2],
+            yMin = fGraphBoundingBox[3], yMax = fGraphBoundingBox[1];
+
+        // x-axis
+        var xTickMax = xMax - xMax%5.0;
+        var xTick2 = xTickMax - 5.0;
+        var xTick1 = xTickMax - 10.0;
+        var xTickMin = xTickMax - 15.0;
+
+        fGraphxAxisTicks.push(fGraphBoard.create(
+            'line',
+            [[xTickMax, yMin], [xTickMax, yMax]],
+            {
+                strokeColor:'#ccc',
+                dash: 1,
+                fixed: true,
+                highlight: false
+            }
+        ));
+
+        fGraphxAxisTicks.push(fGraphBoard.create(
+            'line',
+            [[xTick2, yMin], [xTick2, yMax]],
+            {
+                strokeColor:'#ccc',
+                dash: 1,
+                fixed: true,
+                highlight: false
+            }
+        ));
+
+        fGraphxAxisTicks.push(fGraphBoard.create(
+            'line',
+            [[xTick1, yMin], [xTick1, yMax]],
+            {
+                strokeColor:'#ccc',
+                dash: 1,
+                fixed: true,
+                highlight: false
+            }
+        ));
+
+        fGraphxAxisTicks.push(fGraphBoard.create(
+            'line',
+            [[xTickMin, yMin], [xTickMin, yMax]],
+            {
+                strokeColor:'#ccc',
+                dash: 1,
+                fixed: true,
+                highlight: false
+            }
+        ));
+
+        fGraphxAxisTicks.push(fGraphBoard.create(
+            'text',
+            [
+                function() {return fGraphxAxisTicks[0].point1.X() + 0.25;},
+                function() {return fGraphxAxisTicks[0].point1.Y() + 1.0;},
+                function() {return fGraphxAxisTicks[0].point1.X()}
+            ],
+            {
+                anchorX: 'left',
+                fixed:true
+            }
+        ));
+
+        fGraphxAxisTicks.push(fGraphBoard.create(
+            'text',
+            [
+                function() {return fGraphxAxisTicks[1].point1.X() + 0.25;},
+                function() {return fGraphxAxisTicks[1].point1.Y() + 1.0;},
+                function() {return fGraphxAxisTicks[1].point1.X()}
+            ],
+            {
+                anchorX: 'left',
+                fixed:true
+            }
+        ));
+
+        fGraphxAxisTicks.push(fGraphBoard.create(
+            'text',
+            [
+                function() {return fGraphxAxisTicks[2].point1.X() + 0.25;},
+                function() {return fGraphxAxisTicks[2].point1.Y() + 1.0;},
+                function() {return fGraphxAxisTicks[2].point1.X()}
+            ],
+            {
+                anchorX: 'left',
+                fixed:true
+            }
+        ));
+
+        fGraphxAxisTicks.push(fGraphBoard.create(
+            'text',
+            [
+                function() {return fGraphxAxisTicks[3].point1.X() + 0.25;},
+                function() {return fGraphxAxisTicks[3].point1.Y() + 1.0;},
+                function() {return fGraphxAxisTicks[3].point1.X()}
+            ],
+            {
+                anchorX: 'left',
+                fixed:true
+            }
+        ));
+
+        // y-axis
+        var yTickMax = yMax - yMax%5.0;
+        var yTick2 = yTickMax - 5.0;
+        var yTick1 = yTickMax - 10.0;
+        var yTickMin = yTickMax - 15.0;
+
+        fGraphyAxisTicks.push(fGraphBoard.create(
+            'line',
+            [[xMin, yTickMax], [xMax, yTickMax]],
+            {
+                strokeColor:'#ccc',
+                dash: 1,
+                fixed: true,
+                highlight: false
+            }
+        ));
+
+        fGraphyAxisTicks.push(fGraphBoard.create(
+            'line',
+            [[xMin, yTick2], [xMax, yTick2]],
+            {
+                strokeColor:'#ccc',
+                dash: 1,
+                fixed: true,
+                highlight: false
+            }
+        ));
+
+        fGraphyAxisTicks.push(fGraphBoard.create(
+            'line',
+            [[xMin, yTick1], [xMax, yTick1]],
+            {
+                strokeColor:'#ccc',
+                dash: 1,
+                fixed: true,
+                highlight: false
+            }
+        ));
+
+        fGraphyAxisTicks.push(fGraphBoard.create(
+            'line',
+            [[xMin, yTickMin], [xMax, yTickMin]],
+            {
+                strokeColor:'#ccc',
+                dash: 1,
+                fixed: true,
+                highlight: false
+            }
+        ));
+
+        fGraphyAxisTicks.push(fGraphBoard.create(
+            'text',
+            [
+                function() {return fGraphyAxisTicks[0].point1.X() + 0.5;},
+                function() {return fGraphyAxisTicks[0].point1.Y() - 1.0;},
+                function() {return fGraphyAxisTicks[0].point1.Y()}
+            ],
+            {
+                anchorX: 'left',
+                fixed:true
+            }
+        ));
+
+        fGraphyAxisTicks.push(fGraphBoard.create(
+            'text',
+            [
+                function() {return fGraphyAxisTicks[1].point1.X() + 0.5;},
+                function() {return fGraphyAxisTicks[1].point1.Y() - 1.0;},
+                function() {return fGraphyAxisTicks[1].point1.Y()}
+            ],
+            {
+                anchorX: 'left',
+                fixed:true
+            }
+        ));
+
+        fGraphyAxisTicks.push(fGraphBoard.create(
+            'text',
+            [
+                function() {return fGraphyAxisTicks[2].point1.X() + 0.5;},
+                function() {return fGraphyAxisTicks[2].point1.Y() - 1.0;},
+                function() {return fGraphyAxisTicks[2].point1.Y()}
+            ],
+            {
+                anchorX: 'left',
+                fixed:true
+            }
+        ));
+
+        /*
+        fGraphyAxisTicks.push(fGraphBoard.create(
+            'text',
+            [
+                function() {return fGraphyAxisTicks[3].point1.X() + 0.5;},
+                function() {return fGraphyAxisTicks[3].point1.Y() - 1.0;},
+                function() {return fGraphyAxisTicks[3].point1.Y()}
+            ],
+            {
+                anchorX: 'left',
+                fixed:true
+            }
+        ));
+        */
+
+        /*fGraphxAxis = fGraphBoard.create('axis', [[0.0, 0.0], [1.0, 0.0]], {
+            //withLabel: false,
+            //drawZero: true
+        });*/
+        //fGraphxAxis.removeAllTicks();
+        /*fGraphBoard.create('ticks', [fGraphxAxis, 10], { // The number here is the distance between Major ticks
+            strokeColor:'#ccc',
+            majorHeight: -1, // -1 Need this because the JXG.Options one doesn't apply
+            drawLabels: true, // Needed, and only works for equidistant ticks
+            label: {offset: [20, 25]},
+            minorTicks: 5, // The NUMBER of small ticks between each Major tick
+            drawZero: true,
+            precision: 10
+        }
+        );*/
 
 
-        fGraphyAxis = fGraphBoard.create('axis', [[0.0, 0.0], [0.0, 1.0]], {
-            withLabel: false,
-            grid: false
+        /*fGraphyAxis = fGraphBoard.create('axis', [[0.0, 0.0], [0.0, 1.0]], {
+            //withLabel: false,
+            //grid: false
         });
         fGraphyAxis.removeAllTicks();
+        fGraphBoard.create('ticks', [fGraphyAxis, 10], { // The number here is the distance between Major ticks
+            strokeColor:'#ccc',
+            majorHeight: -1, // -1 Need this because the JXG.Options one doesn't apply
+            drawLabels: true, // Needed, and only works for equidistant ticks
+            label: {offset: [25, 10]},
+            // minorTicks: 5, // The NUMBER of small ticks between each Major tick
+            drawZero: true,
+            precision: 10
+        }
+        );*/
 
         fGraphBoard.create(
             'functiongraph',
@@ -220,6 +494,7 @@ var DataEntry = (function($, _, JXG, undefined) {
             grid: false
         });
 
+        /*
         gGraphxAxis = gGraphBoard.create('axis', [[0.0, 0.0], [1.0, 0.0]], {
             withLabel: false
         });
@@ -229,6 +504,7 @@ var DataEntry = (function($, _, JXG, undefined) {
             withLabel: false
         });
         gGraphyAxis.removeAllTicks();
+        */
 
         gGraphBoard.create(
             'functiongraph',
